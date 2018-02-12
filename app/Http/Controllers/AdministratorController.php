688ecf7f17ccaf0f48ec753af4459aa8530fb8f1
Fabian <fabiing10@gmail.com>
2018-02-12 18:17:23 -0500

    Update report sedes

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -125,8 +125,9 @@ class AdministratorController extends Controller
 
     public function getUsers(){
         $user = Auth::user();
-        $usersMen = User::where('userId',$user->id)->where('gender','masculino')->where('id','!=',$user->id)->where('contactType','ministerio')->get();
-        $usersWomen = User::where('userId',$user->id)->where('gender','femenino')->where('id','!=',$user->id)->where('contactType','ministerio')->get();
+
+
+
         $usersGeneral = User::where('leaderPrincipal',$user->id)->where('level',12)->get();
 
         $usersHeadquarters = User::where('contactType','sede')->get();
@@ -134,194 +135,36 @@ class AdministratorController extends Controller
         $usersTotal = User::all()->count();
         $usersTotalCount = $usersTotal - 1;
 
-        $users12 = User::where('userId',$user->id)->select('id','name')->get();
-
-        $contactos12Array = array();
-        $grupo12Array = array();
-
-        foreach($users12 as $ups12){
-          $g12 = User::where('userId',$ups12->id)->where('id','!=',$ups12->id)->where('contactType','ministerio')->select('id','name')->count();
-
-          if($g12 != 0){
-             $g12select1 = User::where('userId',$ups12->id)->where('id','!=',$ups12->id)->where('contactType','ministerio')->select('id','name')->get();
-             foreach($g12select1 as $gs12){
-               $g12level1 = User::where('userId',$gs12->id)->where('id','!=',$gs12->id)->where('contactType','ministerio')->select('id','name')->count();
-               $g12 = $g12level1 + $g12;
-             }
-          }
-
-          $c12 = User::where('userId',$ups12->id)->where('id','!=',$ups12->id)->where('contactType','contacto')->select('id','name')->count();
-
-          if($c12 != 0){
-             $c12select1 = User::where('userId',$ups12->id)->where('id','!=',$ups12->id)->where('contactType','ministerio')->select('id','name')->get();
-             foreach($c12select1 as $cs12){
-               $c12level1 = User::where('userId',$cs12->id)->where('id','!=',$cs12->id)->where('contactType','ministerio')->select('id','name')->count();
-               $c12 = $c12level1 + $c12;
-             }
-          }
-
-          array_push($grupo12Array,$g12);
-          array_push($contactos12Array,$c12);
-
-        }
-
-        $arrayUsersPrincipal = array();
-        foreach($users12 as $u12){
-          array_push($arrayUsersPrincipal,$u12->name);
-        }
-
-        $hBar12 =
-         app()->chartjs
-         ->name('ChartTest')
-         ->type('horizontalBar')
-         ->size(['width' => 270, 'height' => 200])
-         ->labels($arrayUsersPrincipal)
-         ->datasets([
-             [
-                 "label" => "Equipo",
-                 'backgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00', '#61C80D', '#7AD11A', '#93DA27', '#ACE334', '#C5EC41', '#DEF54E', '#F8FF5C'],
-                 'hoverBackgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00', '#61C80D', '#7AD11A', '#93DA27', '#ACE334', '#C5EC41', '#DEF54E', '#F8FF5C'],
-                 'data' => $grupo12Array
-             ],
-             [
-                 "label" => "Contactos",
-                 'backgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00', '#61C80D', '#7AD11A', '#93DA27', '#ACE334', '#C5EC41', '#DEF54E', '#F8FF5C'],
-                 'hoverBackgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00', '#61C80D', '#7AD11A', '#93DA27', '#ACE334', '#C5EC41', '#DEF54E', '#F8FF5C'],
-                 'data' => $contactos12Array
-             ],
-         ])
-         ->optionsRaw([
-             'legend' => [
-                 'display' => false,
-             ],
-         ]);
 
 
-      /*   $l1 = User::where('location','Antonio Nariño')->count();
-        $l2 = User::where('location','Barrios Unidos')->count();
-        $l3 = User::where('location','Bosa')->count();
-        $l4 = User::where('location','Chapinero')->count();
-        $l5 = User::where('location','Ciudad Bolivar')->count();
-        $l6 = User::where('location','Engativa')->count();
-        $l7 = User::where('location','Fontibon')->count();
-        $l8 = User::where('location','Kennedy')->count();
-        $l9 = User::where('location','La Candelaria')->count();
-        $l10 = User::where('location','Los Martires')->count();
-        $l11 = User::where('location','Puente Aranda')->count();
-        $l12 = User::where('location','Rafael Uribe Uribe')->count();
-        $l13 = User::where('location','San Cristobal')->count();
-        $l14 = User::where('location','Santa Fe')->count();
-        $l15 = User::where('location','Sumapaz')->count();
-        $l16 = User::where('location','Teusaquillo')->count();
-        $l17 = User::where('location','Tunjuelito')->count();
-        $l18 = User::where('location','Usaquen')->count();
-        $l19 = User::where('location','Usme')->count();
-        $l20 = User::where('location','Engativa')->count();
-
-        $l21 = User::where('location','Soacha')->count();
-        $l22 = User::where('location','Mosquera')->count();
-        $l23 = User::where('location','Madrid')->count();
-        $l24 = User::where('location','Chia')->count();
-        $l25 = User::where('location','Cajica')->count();
-
-
-
-         $chartjs =
-          app()->chartjs
-          ->name('pieChartTest')
-          ->type('pie')
-          ->size(['width' => 390, 'height' => 230])
-          ->labels(['Antonio Nariño', 'Barrios Unidos', 'Bosa', 'Chapinero', 'Ciudad Bolivar', 'Engativa', 'Fontibon', 'Kennedy', 'La Candelaria', 'Los Martires', 'Puente Aranda', 'Rafael Uribe', 'San Cristobal', 'Santa Fe', 'Suba', 'Sumapaz', 'Teusaquillo', 'Tunjuelito', 'Usaquen', 'Usme', 'Municipio - Soacha', 'Municipio - Mosquera', 'Municipio - Madrid', 'Municipio - Chia', 'Municipio - Cajica'])
-          ->datasets([
-              [
-                  "label" => "Contactos",
-                  'backgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00', '#62C601', '#7CCE02', '#96D408', '#B0DA0F', '#CAE016', '#E4E61D', '#FFED24'],
-                  'hoverBackgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00', '#62C601', '#7CCE02', '#96D408', '#B0DA0F', '#CAE016', '#E4E61D', '#FFED24'],
-                  'data' => [$l1,$l2,$l3,$l4,$l5,$l6,$l7,$l8,$l9,$l10,$l11,$l12,$l13,$l14,$l15,$l16,$l17,$l18,$l19,$l20,$l21,$l22,$l23,$l24,$l25]
-              ],
-          ])
-          ->optionsRaw([
-              'legend' => [
-                  'display' => true,
-                  'position' => 'left',
-
-              ],
-          ]);
-
-
-          $usersPrincipal = User::where('leaderPrincipal',$user->id)->where('id','!=',$user->id)->select('id','name')->get();
-
-          $contactosArray = array();
-          $grupoArray = array();
-
-          foreach($usersPrincipal as $ups){
-            $g = User::where('leaderPrincipal',$ups->id)->where('id','!=',$ups->id)->where('contactType','ministerio')->select('id','name')->count();
-            $c = User::where('leaderPrincipal',$ups->id)->where('id','!=',$ups->id)->where('contactType','contacto')->select('id','name')->count();
-            array_push($grupoArray,$g);
-            array_push($contactosArray,$c);
-          }
-
-
-          $arrayUsersPrincipal = array();
-          foreach($usersPrincipal as $up){
-            array_push($arrayUsersPrincipal,$up->name);
-          }
-
-         $hBar =
-           app()->chartjs
-           ->name('barChartTest')
-           ->type('horizontalBar')
-           ->size(['width' => 270, 'height' => 200])
-           ->labels($arrayUsersPrincipal)
-           ->datasets([
-               [
-                   "label" => "Equipo",
-                   'backgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00'],
-                   'hoverBackgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00'],
-                   'data' => $grupoArray
-               ],
-               [
-                   "label" => "Contactos",
-                   'backgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00'],
-                   'hoverBackgroundColor' => ['#5B2EFF', '#372AFB', '#273AF7', '#2456F3', '#2172EF', '#1E8EEC', '#1BAAE8', '#18C6E4', '#15E0DF', '#12DDBD', '#10D99B', '#0DD579', '#0BD157', '#08CE36', '#06CA16', '#12C604', '#2DC202', '#48BF00'],
-                   'data' => $contactosArray
-               ],
-           ])
-           ->optionsRaw([
-               'legend' => [
-                   'display' => false,
-               ],
-           ]);
-
-        $gender_chart = app()->chartjs
-        ->name('pieChartTest')
-        ->type('pie')
-        ->size(['width' => 400, 'height' => 200])
-        ->labels(['Label x', 'Label y'])
-        ->datasets([
-            [
-                'backgroundColor' => ['#FF6384', '#36A2EB'],
-                'hoverBackgroundColor' => ['#FF6384', '#36A2EB'],
-                'data' => [69, 59]
-            ]
-        ])
-        ->options([]);*/
         if($user->level == 1){
           //Contactos Generales
           $contacts = User::where('contactType','contacto')->count();
           //Valientes Generales
           $valientes = User::where('contactType','ministerio')->count();
+
+          $usersMen = User::where('userId',$user->id)->where('gender','masculino')->where('id','!=',$user->id)->where('contactType','ministerio')->get();
+          $usersWomen = User::where('userId',$user->id)->where('gender','femenino')->where('id','!=',$user->id)->where('contactType','ministerio')->get();
+
         }else{
           if($user->contactType == 'ministerio'){
             //Contactos Generales
             $contacts = User::where('leaderPrincipal',$user->id)->where('contactType','contacto')->count();
             //Valientes Generales
             $valientes = User::where('leaderPrincipal',$user->id)->where('contactType','ministerio')->where('id','!=',$user->id)->count();
+
+            $usersMen = User::where('userId',$user->id)->where('gender','masculino')->where('id','!=',$user->id)->where('contactType','ministerio')->get();
+            $usersWomen = User::where('userId',$user->id)->where('gender','femenino')->where('id','!=',$user->id)->where('contactType','ministerio')->get();
+
           }else{
             //Contactos Generales
             $contacts = User::where('leaderPrincipal',$user->id)->where('contactType','contacto')->count();
             //Valientes Generales
             $valientes = User::where('leaderPrincipal',$user->id)->where('contactType','sede')->where('id','!=',$user->id)->count();
+
+            $usersMen = User::where('userId',$user->id)->where('gender','masculino')->where('id','!=',$user->id)->where('contactType','sede')->get();
+            $usersWomen = User::where('userId',$user->id)->where('gender','femenino')->where('id','!=',$user->id)->where('contactType','sede')->get();
+
           }
 
         }
